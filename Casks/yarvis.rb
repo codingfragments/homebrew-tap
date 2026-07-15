2eebdf2d9b4c9691c71546dcfaa0edb6229d06b2
Stefan Marx <stefan@marx-merten.de>
2026-07-15 17:31:20 +0200

    chore: update yarvis cask to v0.7.4

--- Casks/yarvis.rb
+++ Casks/yarvis.rb
@@ -1,6 +1,6 @@
 cask "yarvis" do
-  version "0.7.3"
-  sha256 "de21484fb8fee80f159e5e05bbcdcc861dbe2f076ef448fc97273a7d8fd1ec9d"
+  version "0.7.4"
+  sha256 "9fb5786760f72d96685fbb72571e1943e506f115dc64fde57fa11017ff393494"
 
   url "https://github.com/codingfragments/yarvis/releases/download/v#{version}/Yarvis_#{version}_aarch64.dmg"
   name "Yarvis"
